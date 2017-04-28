fe7de4a7705e997f9acc89bf991efa01709b9b75
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-04-28 12:58:01 -0700

    script and dockerfile

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var handleRequest = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello Kubernetes!');
+};
+var www = http.createServer(handleRequest);
+www.listen(process.env.PORT || 8080);
